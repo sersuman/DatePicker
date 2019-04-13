2dc809904d2296f43bb3d18ce05c4a3b09ccd6ad
Suman <sumanshrestha670@gmail.com>
2019-04-13 15:31:56 +0545

    time picker also added

--- app/src/main/java/com/example/datepicker/MainActivity.java
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.datepicker;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView tvDate, tvResult;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tvDate, tvResult, tvTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
         tvDate = findViewById(R.id.tvDate);
         tvResult = findViewById(R.id.tvResult);
+        tvTime = findViewById(R.id.tvTime);
 
-        tvDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDate();
-            }
-        });
+        tvDate.setOnClickListener(this);
+        tvTime.setOnClickListener(this);
     }
     private void loadDate(){
         //current date as default
@@ -34,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
+//        open date picker and set current date as default
         DatePickerDialog datePickerDialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -42,5 +42,31 @@ public class MainActivity extends AppCompatActivity {
             }
         },year,month,day);
         datePickerDialog.show();
+    }
+    private void loadTime(){
+//        get current time
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR);
+        int minute = c.get(Calendar.MINUTE);
+        int second = c.get(Calendar.SECOND);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String time = hourOfDay+":"+minute;
+                tvResult.setText(time);
+            }
+        },hour,minute,true);
+        timePickerDialog.show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tvDate){
+            loadDate();
+        }if (v.getId() == R.id.tvTime){
+            loadTime();
+        }
     }
 }
